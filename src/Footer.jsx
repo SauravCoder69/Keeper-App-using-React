060665b94ac6626141dc5b992debb589aae25068
Saurav Jha <sauravjha4575@gmail.com>
2025-08-20 23:59:23 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer>
+              <p>Copyright@2025</p>  
+              </footer>
+    
+    </div>
+  )
+}
+
+export default Footer
